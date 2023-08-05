d64c61aeb2c8cf47a6ff8b14ac72eca5620d7658
junwei <kfwzmail@163.com>
2023-08-06 00:07:35 +0800

    wjw

--- springboot_rabbitmq/src/main/java/com/baiqi/fastjson/service/controller/FastJsonTest.java
+++ springboot_rabbitmq/src/main/java/com/baiqi/fastjson/service/controller/FastJsonTest.java
@@ -1,12 +1,12 @@
 package com.baiqi.fastjson.service.controller;
 
+
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
 import com.baiqi.fastjson.service.model.User;
 import com.baiqi.fastjson.service.model.UserGroup;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -14,9 +14,8 @@ import java.util.List;
 /**
  * @author wjw
  */
+@Slf4j
 public class FastJsonTest {
-    private static final Logger log = LoggerFactory.getLogger(FastJsonTest.class);
-
     /**
      * java对象转 json字符串
      */
